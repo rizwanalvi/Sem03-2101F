66970a1aada641e30081136233b0a02c302736c3
rizwanalvi <rizwan@aptechsite.net>
2022-08-03 20:29:10 +0500

    Session02

---
+++ PHP/Php02/index01.php
@@ -0,0 +1,26 @@
+<?php
+$BookTile = "Java Script";
+$BookPrice = 235.65;
+$Author = "Aptech";
+$Quantity = 10; 
+$img = "<img src='img/js.jpg' height='80px' >";
+echo "<table border='1'>
+<tr>
+<th>Book Name</th>
+<th>Book Price</th>
+<th>Book Quantity</th>
+<th>Book Author</th>
+<th>Poster</th>
+</tr>
+<tr>
+<td>$BookTile</td>
+<td>$BookPrice</td>
+<td>$Author</td>
+<td>$Quantity</td>
+
+<td>$img</td>
+</tr>
+</table>
+";
+?>
+
