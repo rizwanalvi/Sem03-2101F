6f38d3e439cad0a8b1c1480007a2c572542f5994
rizwanalvi <rizwan@aptechsite.net>
2022-08-24 20:19:31 +0500

    ReadData

--- PHP/PHP08/index.php
+++ PHP/PHP08/index.php
@@ -47,51 +47,6 @@ if(isset($_GET['btnSave'])){
 <div class="col-md-4"></div>
 
 </div>
-<?php
-    if($conn){
-        $query ="SELECT * FROM STUDENT";
-       $result = mysqli_query($conn,$query);
-       echo '<table class="table">
-       <thead class="thead-dark">
-         <tr>
-           <th scope="col">Student ID</th>
-           <th scope="col">Name</th>
-           <th scope="col">Batch ID</th>
-           <th scope="col">Fee Status</th>
-           <th scope="col"></th>
-         </tr>
-       </thead>
-       <tbody>';
-      
-      while($row = mysqli_fetch_assoc($result)){
-        $status="";
-        if($row['FEESTATUS'] ==1){
-                $status = "Paid";
 
-        }
-        else{
-
-            $status = "Pending";
-        }
-        echo "<tr>
-        <th >".$row['ID']."</th>
-        <td>".$row['STDNAME']."</td>
-        <td>".$row['BATCHID']."</td>
-        <td>".$status."</td>
-        <td>   <div class='btn btn-group'>
-        <a href='#' class='btn btn-success btn-sm'>Edit</a>
-        <a href='#' class='btn btn-warning btn-sm'>Update</a>
-        <a href='#' class='btn btn-danger btn-sm'>Delete</a>
-       
-       </div></td>
-      </tr>";
-           
-            
-      }
-      echo "    </tbody>
-      </table>";
-     
-    }
-?>
 
 
